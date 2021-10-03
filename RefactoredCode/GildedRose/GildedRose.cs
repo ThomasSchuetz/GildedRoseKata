76e5737749ece92ba63ae39d0d5456ae2f9502df
Thomas Schuetz <thomas.schuetz@eon.com>
2021-10-03 19:03:01 +0200

    Removed old code that has been replaced with strategy pattern

--- RefactoredCode/GildedRose/GildedRose.cs
+++ RefactoredCode/GildedRose/GildedRose.cs
@@ -14,83 +14,8 @@ namespace GildedRose
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                try
-                {
-                    var refactoredItem = ItemFactory.Create(Items[i]);
-                    refactoredItem.Update();
-                }
-                catch (System.NotImplementedException)
-                {
-                    if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Items[i].Quality > 0)
-                        {
-                            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                Items[i].Quality = Items[i].Quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-
-                            if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                            {
-                                if (Items[i].SellIn < 11)
-                                {
-                                    if (Items[i].Quality < 50)
-                                    {
-                                        Items[i].Quality = Items[i].Quality + 1;
-                                    }
-                                }
-
-                                if (Items[i].SellIn < 6)
-                                {
-                                    if (Items[i].Quality < 50)
-                                    {
-                                        Items[i].Quality = Items[i].Quality + 1;
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        Items[i].SellIn = Items[i].SellIn - 1;
-                    }
-
-                    if (Items[i].SellIn < 0)
-                    {
-                        if (Items[i].Name != "Aged Brie")
-                        {
-                            if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                            {
-                                if (Items[i].Quality > 0)
-                                {
-                                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                    {
-                                        Items[i].Quality = Items[i].Quality - 1;
-                                    }
-                                }
-                            }
-                            else
-                            {
-                                Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                            }
-                        }
-                        else
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
-                    }
-                }
+                var refactoredItem = ItemFactory.Create(Items[i]);
+                refactoredItem.Update();
             }
         }
     }
