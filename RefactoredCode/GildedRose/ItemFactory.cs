bc88e7992958017f6d11b243688857b8ab71ea5d
Thomas Schuetz <thomas.schuetz@eon.com>
2021-10-03 18:36:53 +0200

    Added backstage pass

--- RefactoredCode/GildedRose/ItemFactory.cs
+++ RefactoredCode/GildedRose/ItemFactory.cs
@@ -10,6 +10,7 @@ namespace GildedRose
             {
                 "Aged Brie" => new AgedBrie(item),
                 "Sulfuras, Hand of Ragnaros" => new Sulfuras(item),
+                "Backstage passes to a TAFKAL80ETC concert" => new BackstagePass(item),
                 _ => throw new NotImplementedException($"Unknown item: {item.Name}"),
             };
         }
